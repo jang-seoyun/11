698c08665d991d5dc3bc9013ee37a8983d2cd30c
jang-seoyun <rachel2343@naver.com>
2025-11-13 15:43:31 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,21 +2,26 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void swap(int *x, int *y)
-{
-	int temp;
-	temp = *x;
-	*x = *y;
-	*y = temp;
-}
-
 
 int main(int argc, char *argv[]) 
 {
-	int a=3, b=5;
-	int *x, y;
+	int i;
+	int grade[5];
+	int sum=0;
 	
-	swap(&a,&b);
-	printf("a:%i,b:%i\n", a, b);
+	for(i=0;i<5;i++)
+	{
+		printf("grade[%i]=", i);
+		scanf("%d", &grade[i]);
+	}
+	
+	for(i=0;i<5;i++)
+	{
+		printf("grade[%i]=%i\n", i, *(grade+i));
+		sum = sum + *(grade+i);
+	}
+	
+	printf("Average:%i\n", sum/5);
+	
 	return 0;
 }
