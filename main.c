730d3b1f2c04b27ab689616957f9dcab2a67c0b5
jang-seoyun <rachel2343@naver.com>
2025-11-13 15:33:42 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,20 +2,21 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void swap(int x, int y)
+void swap(int *x, int *y)
 {
 	int temp;
-	temp = x;
-	x = y;
-	y = temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
 }
 
 
 int main(int argc, char *argv[]) 
 {
 	int a=3, b=5;
+	int *x, y;
 	
-	swap(a,b);
+	swap(&a,&b);
 	printf("a:%i,b:%i\n", a, b);
 	return 0;
 }
